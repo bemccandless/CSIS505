e5e58ac2ad6969c12e9ac996587efa013e4c2f58
bemccandless <bemccandless@liberty.edu>
2017-03-28 21:33:57 -0400

    buttons work, message displays

---
+++ HW2-Exercise1/src/edu/liberty/bemccandless/csis505/hw2/exercise1/SpamCalculator.java
@@ -0,0 +1,12 @@
+package edu.liberty.bemccandless.csis505.hw2.exercise1;
+
+/**
+ *
+ * @author bemccandless
+ */
+public class SpamCalculator {
+
+    public int calculateSpamCount(String message) {
+        return 2;
+    }
+}
--- HW2-Exercise1/src/edu/liberty/bemccandless/csis505/hw2/exercise1/SpamScoreCalculatorFrame.java
+++ HW2-Exercise1/src/edu/liberty/bemccandless/csis505/hw2/exercise1/SpamScoreCalculatorFrame.java
@@ -3,12 +3,14 @@ package edu.liberty.bemccandless.csis505.hw2.exercise1;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
 import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
@@ -18,14 +20,34 @@ import javax.swing.JTextArea;
  * @author bemccandless
  */
 public class SpamScoreCalculatorFrame extends JFrame {
-
+    
     private final JTextArea messageArea;
     private final JButton clearBtn;
     private final JButton verifyBtn;
+    private static final String SPAMCOUNT_MESSAGE = "The spam score for this message is %s.";
+    private int spamCount;
     
     public SpamScoreCalculatorFrame() {
         super("Spam Score Calculator");
         setLayout(new BorderLayout());
+        
+        messageArea = new JTextArea(20, 50);
+        messageArea.setLineWrap(true);
+        
+        clearBtn = new JButton("Clear");
+        clearBtn.addActionListener((ActionEvent e) -> {
+            messageArea.setText("");
+            messageArea.grabFocus();
+        });
+        
+        SpamCalculator spamCalculator = new SpamCalculator();
+        verifyBtn = new JButton("Verify");
+        verifyBtn.addActionListener((ActionEvent e) -> {
+            spamCount = spamCalculator.calculateSpamCount(messageArea.getText());
+            JOptionPane.showMessageDialog(getContentPane(), String.format(SPAMCOUNT_MESSAGE, spamCount));
+            messageArea.grabFocus();
+            messageArea.selectAll();
+        });
         
         JPanel headerPanel = new JPanel();
         headerPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
@@ -36,8 +58,6 @@ public class SpamScoreCalculatorFrame extends JFrame {
         textPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
         textPanel.add(headerPanel);
         textPanel.add(Box.createRigidArea(new Dimension(0, 5)));
-        messageArea = new JTextArea(20, 50);
-        messageArea.setLineWrap(true);
         JScrollPane textScrollPane = new JScrollPane(messageArea);
         textPanel.add(textScrollPane);
         
@@ -45,8 +65,6 @@ public class SpamScoreCalculatorFrame extends JFrame {
         buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.LINE_AXIS));
         buttonPanel.setBorder(BorderFactory.createEmptyBorder(0, 10, 10, 10));
         buttonPanel.add(Box.createHorizontalGlue());
-        clearBtn = new JButton("Clear");
-        verifyBtn = new JButton("Verify");
         buttonPanel.add(clearBtn);
         buttonPanel.add(Box.createRigidArea(new Dimension(10, 0)));
         buttonPanel.add(verifyBtn);
