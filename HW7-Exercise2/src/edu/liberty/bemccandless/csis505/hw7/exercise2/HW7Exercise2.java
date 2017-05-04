43e8e39a1a6e0bdaf3eaf1b6ef14a3bb1479cfb0
bemccandless <bemccandless@liberty.edu>
2017-05-04 07:22:34 -0400

    init

---
+++ HW7-Exercise2/src/edu/liberty/bemccandless/csis505/hw7/exercise2/HW7Exercise2.java
@@ -0,0 +1,33 @@
+package edu.liberty.bemccandless.csis505.hw7.exercise2;
+
+/**
+ *
+ * @author bemccandless
+ */
+public class HW7Exercise2 {
+
+    public static void main(String[] args) {
+        IndexedList list = new IndexedList();
+        list.print();
+        
+        list.insertInIndexedList("Albert");
+        list.insertInIndexedList("Xena");
+        list.insertInIndexedList("Claire");
+        list.insertInIndexedList("Elijah");
+        list.insertInIndexedList("Eric");
+        list.insertInIndexedList("Isaiah");
+        list.insertInIndexedList("Olivia");
+        
+        list.print();
+        
+        String result = list.searchIndexedList("Eric");
+        
+        if (result != null) {
+            System.out.printf("'%s' was found%n", result);
+        } else {
+            System.out.printf("'%s' was not found%n", result);
+        }
+
+    }
+
+}
---
+++ HW7-Exercise2/src/edu/liberty/bemccandless/csis505/hw7/exercise2/IndexedList.java
@@ -0,0 +1,130 @@
+package edu.liberty.bemccandless.csis505.hw7.exercise2;
+
+class ListNode {
+
+    String data;
+    ListNode nextNode;
+    ListNode indexNode;
+
+    ListNode(String object) { 
+       this(object, null); 
+    }  
+
+    ListNode(String object, ListNode node) {
+       data = object;    
+       nextNode = node;  
+    } 
+
+    String getData() { 
+       return data; 
+    } 
+
+    ListNode getNext() { 
+       return nextNode; 
+    } 
+}
+
+public class IndexedList {
+    
+    private ListNode firstNode;
+    private ListNode lastNode;
+    private String name;
+
+    public IndexedList() { 
+        this("list"); 
+    } 
+
+    public IndexedList(String listName) {
+        name = listName;
+        createIndex();
+    }
+    
+    public String searchIndexedList(String searchItem) {
+        ListNode indexedNode = findNode(String.valueOf(searchItem.charAt(0)));
+        if (indexedNode.nextNode == null) {
+            return null;
+        } else {
+            while (indexedNode.nextNode != null) {
+                if (searchItem.equals(indexedNode.getNext().getData())) {
+                    return indexedNode.getData();
+                }
+                indexedNode = indexedNode.getNext();
+            }
+            return null;
+        }
+
+    }
+
+    public void insertInIndexedList(String insertItem) {
+        ListNode indexedNode = findNode(String.valueOf(insertItem.charAt(0)));
+        if (indexedNode.nextNode == null) {
+            indexedNode.nextNode = new ListNode(insertItem);
+        } else {
+            while (insertItem.equals(indexedNode.getNext().getData())) {
+                indexedNode = indexedNode.getNext();
+            }
+            indexedNode.nextNode = new ListNode(insertItem, indexedNode.indexNode);
+        }
+    }
+
+    public void print() {
+        if (isEmpty()) {
+            System.out.printf("Empty %s%n", name);
+            return;
+        }
+
+        System.out.printf("The %s is: ", name);
+        ListNode current = firstNode;
+
+        while (current != null) {
+            System.out.printf("%s ", current.data);
+            current = current.nextNode;
+        }
+
+        System.out.println();
+    } 
+    
+    private void insertAtFront(String insertItem) {
+        if (isEmpty()) {
+            firstNode = lastNode = new ListNode(insertItem);
+        } else {
+            firstNode = new ListNode(insertItem, firstNode);
+        }
+    } 
+    
+    private void insertIndexAtBack(String insertItem) {
+        if (isEmpty()) {
+            firstNode = lastNode = new ListNode(insertItem);
+        } else {
+            lastNode = lastNode.indexNode = lastNode.nextNode = new ListNode(insertItem);
+        }
+    } 
+
+    private boolean isEmpty() { 
+        return firstNode == null;
+    } 
+    
+    private ListNode findNode(String searchValue) {
+        ListNode currentNode = firstNode;
+        while (currentNode.nextNode != null) {
+            if (currentNode.getData().equals(searchValue)) {
+                return currentNode;
+            } 
+            currentNode = currentNode.nextNode;
+        }
+        
+        return null;
+    }
+    
+    private void createIndex() {
+        String[] letters = {
+            "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", 
+            "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
+        };
+        
+        for (String letter : letters) {
+            insertIndexAtBack(letter);
+        }
+        
+    }
+}
